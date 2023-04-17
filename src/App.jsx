82fda01f10ad2f5d41c5a4c609a17e675680d17b
regina <regina-will-programmieren@web.de>
2023-04-17 13:07:06 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-poppins underline">Hello world!</h1>
         <p>
           <button
             type="button"
