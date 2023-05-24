0addb649f90a3e944236815728e39c9342717f0a
regina <regina-will-programmieren@web.de>
2023-05-24 20:14:56 +0200

    Alles umgebaut, jetzt nur noch Programm.jsx funktioniert.

--- src/app/Program.jsx
+++ src/app/Program.jsx
@@ -1,10 +1,15 @@
-import { useParams } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
-import { ProgramDetail } from "../components/ProgramDetail";
-import { QueryResults } from "../components/QueryResults";
+import { Link, useParams } from "react-router-dom";
+import xclose, {
+  ReactComponent as XcloseIcon,
+} from "../images/svg/X-close.svg";
+// import styled from '"@emotion/styled"';
+// import { LoadingSpinner } from "@apollo/space-kit/Loaders/LoadingSpinner";
+// { ProgramDetail } from "../components/ProgramDetail";
+// import { QueryResults } from "../components/QueryResults";
 
 // const PROGRAMS = gql`
-export const GET_PROGRAMS = gql`
+const GET_PROGRAMS = gql`
   query getProgram($id: ID!) {
     program(where: { id: $id }) {
       id
@@ -20,9 +25,6 @@ export const GET_PROGRAMS = gql`
         duration
         colorStylew
       }
-      image {
-        url
-      }
     }
   }
 `;
@@ -34,14 +36,121 @@ const Program = () => {
   const { data, loading, error } = useQuery(GET_PROGRAMS, {
     variables: { id: programId },
   });
-  console.log(data);
   console.log(data, loading, error);
+  if (loading) {
+    return (
+      <div
+        className="text-light mb-4 pt-16 px-4 py-3 shadow-light
+    text-center"
+      >
+        LOADING
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <p
+        className="text-light mb-4 pt-16 px-4 py-3 shadow-light
+    text-center"
+      >
+        ERROR: {error.message}
+      </p>
+    );
+  }
+  if (!data) {
+    return <p>Nothing to show...</p>;
+  }
+  const { program } = data;
+  const { workouts } = program;
 
   return (
-    <QueryResults error={error} loading={loading} data={data}>
-      <ProgramDetail program={data?.program} />
-    </QueryResults>
+    <div>
+      <Link to="/browser" className="fixed top-5 right-5">
+        <XcloseIcon />
+      </Link>
+      <div className={`${program.colorStyle} h-[75vh] w-screen text-light`}>
+        <h1 className=" text-center pt-60">{program.name}</h1>
+        <div className="flex justify-around gap-5 pt-32 ">
+          <div className="flex flex-col items-center">
+            <img src="../images/svg/Ellipse 1.svg"></img>
+            <p className="mb-4 mt-2 text-center uppercase">{program.focus}</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <img src="../images/svg/Ellipse 1.svg"></img>
+            <p className="mb-4 mt-2 text-center uppercase text-light">
+              {program.difficulty}
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <img src="../images/svg/Ellipse 1.svg"></img>
+            <p className="mb-4 mt-2 text-center uppercase">
+              {program.duration} Wochen
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Link
+          to={`/workout/${program.workouts[0].id}`}
+          className={`${program.colorStyle} text-light rounded-3xl fixed px-4 py-3 bottom-8 shadow-md z-[12]`}
+        >
+          <p>jetzt starten</p>
+        </Link>
+      </div>
+      <div className="bg-medium_dark">
+        <p className="px-6 py-5 text-light">{program.description}</p>
+      </div>
+      <div className="px-6 mt-6 bg-dark text-light">
+        <h3>So ist das Programm aufgeteilt:</h3>
+        <div className="flex items-center mt-10 gap-5">
+          <img src="../images/svg/Figpie.svg"></img>
+          <div className="ml-6 flex flex-col gap-5">
+            <div className="flex gap-3">
+              <img src="../images/svg/Ellipse 6.svg"></img>
+              <p className="text-xs">Krafttraining</p>
+            </div>
+            <div className="flex gap-3">
+              <img src="../images/svg/Ellipse 6.svg"></img>
+              <p className="text-xs">Koordination</p>
+            </div>
+            <div className="flex gap-3">
+              <img src="../images/svg/Ellipse 6.svg"></img>
+              <p className="text-xs">Cardio</p>
+            </div>
+            <div className="flex gap-3">
+              <img src="../images/svg/Ellipse 6.svg"></img>
+              <p className="text-xs">Beweglichkeit</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-14 px-6 py-4 flex justify-between items-baseline text-light">
+        <h3>{workouts.length} Tage</h3>
+        <button className="text-xs bg-dark">Alle anzeigen</button>
+      </div>
+      <div>
+        <div className="text-light h-[100px] w-[335px] ml-5 mr-9 my-4 mb-40">
+          {workouts.map((workout, index) => (
+            <div key={`workout-${index}`} className="flex  justify-center mb-3">
+              <div
+                className={`${workout.colorStylew} inline-block rounded-l-2xl w-1/4 h-[100px]`}
+              ></div>
+              <div className="bg-medium_dark inline-block rounded-r-2xl w-3/4 pr-6 h-[100px]">
+                <div className="ml-3.5 py-2.5">
+                  <h3>Tag {index + 1}</h3>
+                  <p className="text-xs mt-6">{workout.duration} Min.</p>
+                  <p className="text-xs">{workout.category}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
-console.log(Program);
+
 export default Program;
+/*     <div className=" flex justify-center align-middle w-3 h-3">
+        <LoadingSpinner data-testid="spinner" size="large" theme="grayscale" />;
+      </div> */
